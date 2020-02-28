eec33584da7856c8819957e747de0863a2c06236
ZhouLan <1832311982@qq.com>
2020-02-28 22:58:05 +0800

    从头到尾打印单链表

--- lcof/src/$6.java
+++ lcof/src/$6.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Stack;
 
@@ -14,7 +15,7 @@ public class $6 {
     //栈
     public static int[] reversePrint(ListNode head) {
         if (head == null) {
-            return null;
+            return new int[]{};
         }
         if (head.next == null) {
             return new int[]{head.val};
@@ -30,28 +31,53 @@ public class $6 {
         }
         return res;
     }
+
     //递归
-    //原地改变结点指向
+    public static ArrayList<Integer> tmp = new ArrayList<>();
+    public static int[] reversePrint2(ListNode head) {
+        if (head == null) {
+            return new int[]{};
+        }
+        if (head.next == null) {
+            return new int[]{head.val};
+        }
+        recur(head);
+        int[] res = new int[tmp.size()];
+        for (int i = 0; i < res.length; i++) {
+            res[i] = tmp.get(i);
+        }
+        return res;
+    }
+
+    public static void recur(ListNode head) {
+        if (head == null) {
+            return;
+        }
+        recur(head.next);
+        tmp.add(head.val);
+    }
+
+    //逆置单链表 原地改变结点指向
     public static int[] reversePrint3(ListNode head) {
         ListNode cur = head;
-        ListNode pNext = cur.next;
-        ListNode newHead;
+        ListNode pre = null;
+
+        ListNode newHead = null;
 
         int cnt = 0;
-        while (true) {
-            ListNode tmp = pNext;
-            pNext.next = cur;
-            cur = tmp;
-            pNext = cur.next;
-            cnt++;
-            if (cur.next == null) {
-                newHead = cur;
-                break;
+        while (cur != null) {
+            ListNode tmp = cur.next;
+            if (tmp == null) {
+                newHead = cur; //后面不能加 break，不然 cur 的指向就没有了
             }
+            cur.next = pre;
+            pre = cur;
+            cur = tmp;
+            cnt++;
         }
         int[] res = new int[cnt];
         int i = 0;
-        while (newHead.next != null) {
+        while (newHead != null) {
             res[i++] = newHead.val;
             newHead = newHead.next;
         }
@@ -65,6 +91,6 @@ public class $6 {
         head.next = three;
         three.next = two;
 
-        System.out.println(Arrays.toString(reversePrint(head)));
+        System.out.println(Arrays.toString(reversePrint3(head)));
     }
 }
